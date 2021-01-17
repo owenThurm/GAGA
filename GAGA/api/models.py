cac14d7379f24576674e7d1572bbf0ee23b8dd83
owenThurm <owen.p.thurm@gmail.com>
2021-01-17 14:32:47 -0500

    removed duplicate requirements for email field

--- GAGA/api/models.py
+++ GAGA/api/models.py
@@ -44,7 +44,7 @@ class User(AbstractBaseUser):
   brand_name = models.CharField(max_length=20)
 
   USERNAME_FIELD = "email"
-  REQUIRED_FIELDS = ["username", "email", "brand_name", "password"]
+  REQUIRED_FIELDS = ["username", "brand_name", "password"]
 
   objects = UserManager()
 
