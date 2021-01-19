94e405d768782249e068dc3d85939fc6dad5e467
owenThurm <owen.p.thurm@gmail.com>
2021-01-18 19:12:13 -0500

    Changed File Comment

--- GAGA/api/models.py
+++ GAGA/api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
-# Create your models here.
+# Growth Automation Models
 
 class UserManager(BaseUserManager):
 
