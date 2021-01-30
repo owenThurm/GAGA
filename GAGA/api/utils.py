7734c9d5f075f891cab44dee49eb4b03c9dd8a0a
owenThurm <owen.p.thurm@gmail.com>
2021-01-30 15:11:39 -0500

    Fixed overwrite issues

--- GAGA/api/utils.py
+++ GAGA/api/utils.py
@@ -4,6 +4,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GAGA.settings')
 django.setup()
 
+from .services.promo_account_service import PromoAccountService
 from redis import Redis
 from datetime import datetime, timedelta
 import pytz
@@ -14,32 +15,37 @@ import requests
 from .models import User
 from .models import Promo_Account
 from .models import Commented_On_Account
-from .services.promo_account_service import PromoAccountService
+import logging
 
 LAMBDA_URL = 'https://7r2oqaxnxb.execute-api.us-east-1.amazonaws.com/default/InstaBot'
+
+logging.basicConfig(filename='/logs/workerlogs', level=logging.DEBUG)
 
 redis_server = Redis()
 queue = Queue(connection=redis_server)
 
 def add_to_queue(promo_username, promo_password, promo_target, promo_proxy):
-  print(f'adding {promo_username} to queue at ')
+  logging.debug(f'adding {promo_username} to queue at ')
   queue.enqueue_in(timedelta(minutes=0), comment_round, promo_username, promo_password, promo_target, promo_proxy)
 
 def comment_round(promo_username, promo_password, promo_target, promo_proxy):
-  print('called func<<<<<<')
-  print(f'''Comment round for {promo_username} with password: {promo_password},
+  logging.debug('called func<<<<<<')
+  logging.debug(f'''Comment round for {promo_username} with password: {promo_password},
   targeting: {promo_target}, with proxy: {promo_proxy}''')
 
   promo_account = Promo_Account.objects.get(promo_username=promo_username)
-  print(promo_account.user)
+  logging.debug('promo account user: ',  promo_account.user)
 
   if not promo_account.is_queued:
     return
 
-  accounts_already_commented_on = list(promo_account.user.commented_on_account_set.all())
+  accounts_already_commented_on = []
+
+  for commented_on_account in promo_account.user.commented_on_account_set.all():
+    accounts_already_commented_on.append(commented_on_account.commented_on_account_username)
 
   comment_rounds_today = promo_account.comment_rounds_today
-  print('Comment Rounds Today: ', comment_rounds_today)
+  logging.debug('Comment Rounds Today: ', comment_rounds_today)
 
   #to run at = response from aws -> get the finish time from the last comment
   promo_attributes = {
@@ -50,14 +56,13 @@ def comment_round(promo_username, promo_password, promo_target, promo_proxy):
     'accounts_already_commented_on': accounts_already_commented_on
   }
 
-  print(promo_account.activated)
-
   if promo_account.activated:
-    print('comment round ran>>>')
-    #requests.post(LAMBDA_URL, json=promo_attributes)
+    logging.debug('comment round ran>>>')
+    requests.post(LAMBDA_URL, json=promo_attributes)
 
   promo_account.comment_rounds_today += 1
   promo_account.save()
+  logging.debug('comment rounds today: ', promo_account.comment_rounds_today)
   sleep_until_tomorrow = False
   if promo_account.comment_rounds_today >= 8:
     sleep_until_tomorrow = True
@@ -67,9 +72,9 @@ def comment_round(promo_username, promo_password, promo_target, promo_proxy):
   continue_queue(promo_username, promo_password, promo_target, promo_proxy, sleep_until_tomorrow)
 
 def continue_queue(promo_username, promo_password, promo_target, promo_proxy, sleep_until_tomorrow):
-  print('continuing queue')
+  logging.debug('continuing queue')
   if sleep_until_tomorrow:
-    queue.enqueue_in(timedelta(hours=13, minutes=randint(10,50)), comment_round, promo_username, promo_password, promo_target, promo_proxy)
+    queue.enqueue_in(timedelta(hours=13, minutes=randint(10, 50)), comment_round, promo_username, promo_password, promo_target, promo_proxy)
 
   else:
-    queue.enqueue_in(timedelta(seconds=20), comment_round, promo_username, promo_password, promo_target, promo_proxy)
+    queue.enqueue_in(timedelta(minutes=randint(80,100)), comment_round, promo_username, promo_password, promo_target, promo_proxy)
